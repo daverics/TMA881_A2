6ec689371f9cbb4685cf4cabc5a6f6ec4cedc3bc
daverics <david.a.ericsson@gmail.com>
2018-10-05 11:44:43 +0200

    :i Adding function to concat strings of different length

---
+++ concatDiffString.h
@@ -0,0 +1,18 @@
+char *concatDiffString(int arg c, char * argv[]) {
+     int length = 0;
+     for (int i = 0; i < argc; ++i)
+         length += strlen(argv[i]);
+
+
+     char *output = (char*)malloc(length + 1);
+
+
+     char *dest = output;
+     for (int i = 0; i < argc; ++i) {
+         char *src = argv[i];
+         while (*src)
+             *dest++ = *src++;
+     }
+     *dest = '\0';
+     return output;
+}
